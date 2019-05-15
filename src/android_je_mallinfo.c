88dfa30f13c945f4e88f022038e31b2503baac5d
mydongistiny <jaysonedson@gmail.com>
2019-05-14 19:10:10 -0700

    jemalloc: update to version 5.2.0
    Fixed Android specific code for update as well
    
    Change-Id: Iaf26bb265c1c1b46ff1a4c3c3442f768632af7c4
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/android_je_mallinfo.c
+++ src/android_je_mallinfo.c
@@ -28,10 +28,9 @@ struct mallinfo je_mallinfo() {
       mi.hblkhd += atomic_load_zu(&arena->stats.mapped, ATOMIC_ACQUIRE);
       mi.uordblks += atomic_load_zu(&arena->stats.allocated_large, ATOMIC_ACQUIRE);
 
-      for (unsigned j = 0; j < NBINS; j++) {
-        bin_t* bin = &arena->bins[j];
-
-        malloc_mutex_lock(TSDN_NULL, &bin->lock);
+      for (unsigned j = 0; j < SC_NBINS; j++) {
+        unsigned binshard;
+        bin_t *bin = arena_bin_choose_lock(TSDN_NULL, arena, j, &binshard);
         mi.uordblks += bin_infos[j].reg_size * bin->stats.curregs;
         malloc_mutex_unlock(TSDN_NULL, &bin->lock);
       }
@@ -44,11 +43,11 @@ struct mallinfo je_mallinfo() {
 }
 
 size_t __mallinfo_narenas() {
-  return narenas_auto;
+  return manual_arena_base;
 }
 
 size_t __mallinfo_nbins() {
-  return NBINS;
+  return SC_NBINS;
 }
 
 struct mallinfo __mallinfo_arena_info(size_t aidx) {
@@ -56,16 +55,15 @@ struct mallinfo __mallinfo_arena_info(size_t aidx) {
   memset(&mi, 0, sizeof(mi));
 
   malloc_mutex_lock(TSDN_NULL, &arenas_lock);
-  if (aidx < narenas_auto) {
+  if (aidx < manual_arena_base) {
     arena_t* arena = atomic_load_p(&arenas[aidx], ATOMIC_ACQUIRE);
     if (arena != NULL) {
       mi.hblkhd = atomic_load_zu(&arena->stats.mapped, ATOMIC_ACQUIRE);
       mi.ordblks = atomic_load_zu(&arena->stats.allocated_large, ATOMIC_ACQUIRE);
 
-      for (unsigned j = 0; j < NBINS; j++) {
-        bin_t* bin = &arena->bins[j];
-
-        malloc_mutex_lock(TSDN_NULL, &bin->lock);
+      for (unsigned j = 0; j < SC_NBINS; j++) {
+        unsigned binshard;
+        bin_t *bin = arena_bin_choose_lock(TSDN_NULL, arena, j, &binshard);
         mi.fsmblks += bin_infos[j].reg_size * bin->stats.curregs;
         malloc_mutex_unlock(TSDN_NULL, &bin->lock);
       }
@@ -80,12 +78,11 @@ struct mallinfo __mallinfo_bin_info(size_t aidx, size_t bidx) {
   memset(&mi, 0, sizeof(mi));
 
   malloc_mutex_lock(TSDN_NULL, &arenas_lock);
-  if (aidx < narenas_auto && bidx < NBINS) {
+  if (aidx < manual_arena_base && bidx < SC_NBINS) {
     arena_t* arena = atomic_load_p(&arenas[aidx], ATOMIC_ACQUIRE);
     if (arena != NULL) {
-      bin_t* bin = &arena->bins[bidx];
-
-      malloc_mutex_lock(TSDN_NULL, &bin->lock);
+      unsigned binshard;
+      bin_t *bin = arena_bin_choose_lock(TSDN_NULL, arena, (uint32_t) bidx, &binshard);
       mi.ordblks = bin_infos[bidx].reg_size * bin->stats.curregs;
       mi.uordblks = (size_t) bin->stats.nmalloc;
       mi.fordblks = (size_t) bin->stats.ndalloc;
